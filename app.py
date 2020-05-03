dfbfc044f618c3446a97c3d36a8093dfce170d3c
attkauppi <ari.kauppi@helsinki.fi>
2020-05-03 07:24:56 +0300

    Tyylin ottaminen layout.html:stä index.html'ään ainakin onnistuu

--- app.py
+++ app.py
@@ -8,42 +8,14 @@ import json
 
 app = Flask(__name__)
 
-
-
-def create_plot():
-
-
-    N = 40
-    x = np.linspace(0, 1, N)
-    y = np.random.randn(N)
-    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
-
-
-    data = [
-        go.Bar(
-            x=df['x'], # assign x as the dataframe column 'x'
-            y=df['y']
-        )
-    ]
-
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return graphJSON
-
 @app.route('/')
 def index():
-    #return "Hello world!"
-    bar = create_plot()
-    return render_template('index.html', plot=bar)
+    #return "Hello world!" 
+    #bar = create_plot()
+    return render_template('index.html')
+    #return render_template('index.html', plot=bar)
 
 
-@app.route('/bar', methods=['GET', 'POST'])
-def change_features():
-
-    feature = request.args['selected']
-    graphJSON= create_plot(feature)
-
-    return graphJSON
 
 if __name__=='__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
